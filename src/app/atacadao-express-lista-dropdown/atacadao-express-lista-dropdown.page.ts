31a289cb459241e0bbf296f5689492b16df2b590
zabella-12 <issaasousa@outlook.com>
2021-12-11 20:00:30 -0300

    inclusão de pagamento e listas

--- src/app/atacadao-express-lista-dropdown/atacadao-express-lista-dropdown.page.ts
+++ src/app/atacadao-express-lista-dropdown/atacadao-express-lista-dropdown.page.ts
@@ -8,6 +8,31 @@ import { Component, OnInit } from '@angular/core';
 export class AtacadaoExpressListaDropdownPage implements OnInit {
 
   constructor() { }
+  public produto: number = 0;
+  public desconto: any = 0
+  public resultado: number = 0;
+
+  soma(){
+    
+    this.resultado++;
+    this.calcular()
+  }
+  subtrair(){
+    if (this.resultado > 0 ){
+      this.resultado--;
+      this.calcular()
+    }
+  }
+
+calcular(){
+var desc: number = 0.00
+
+ this.produto = 2.5
+ this.produto *= this.resultado
+  
+  this.desconto = (this.produto * 0.05).toFixed(2)
+  
+}  
 
   ngOnInit() {
   }
